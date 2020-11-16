3a631ce738c5437702cf4d5471c33dae826f1c00
Hubert <hubert.kniola@student.put.poznan.pl>
2020-11-16 09:42:17 +0100

    init

---
+++ RSACalculate/RSACalculations.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RSACalculate
+{
+    public class Class1
+    {
+    }
+}
---
+++ RSAlgorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RSAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
